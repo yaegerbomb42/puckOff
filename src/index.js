badb010c22b9b8ba483ef52ba067a4a4c9ca586a
yaegerbomb42 <yaeger.james42@gmail.com>
2026-01-09 23:11:04 -0600

    u

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { AuthProvider } from './contexts/AuthContext';
 import ErrorBoundary from './components/ErrorBoundary';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
